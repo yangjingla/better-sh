5b7e73bfc40fdd032fe52154350b329033b94036
yangjing <yangjing2036@126.com>
2024-06-23 18:09:57 +0800

    [nvidia install]

---
+++ nvidia_install.sh
@@ -0,0 +1,19 @@
+
+
+1. driver
+
+```
+
+wget https://cn.download.nvidia.com/XFree86/Linux-x86_64/550.90.07/NVIDIA-Linux-x86_64-550.90.07.run
+
+```
+
+
+2. cuda
+
+```
+
+wget https://developer.download.nvidia.com/compute/cuda/12.5.0/local_installers/cuda_12.5.0_555.42.02_linux.run
+sudo sh cuda_12.5.0_555.42.02_linux.run
+
+```
