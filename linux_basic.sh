29d53b5df991b503ba6af938f4329808488279a4
yangjing <yangjing2036@126.com>
2024-06-26 11:38:12 +0800

    [add basic command]

--- linux_basic.sh
+++ linux_basic.sh
@@ -10,10 +10,18 @@ tree -Lh 2
 
 
 # ==
-realpath $1 
+realpath filename 
 
 # ==
 time_stamp=$(date +'%Y%m%d_%H:%M:%S')
 
 
 # ==
+
+ls -d "output_*" | xargs rm -f
+
+# ==
+
+awk 'END {print NR}' filename
+
+awk -F, '{print $1, $2}' filename
